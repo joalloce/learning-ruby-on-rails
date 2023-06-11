c55aa3863d1d648839ee108069db28d438a8bcb8
Jose Lo <joalloce@gmail.com>
2023-06-10 23:21:00 -0600

    feat(*): base project

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = "Learning Ruby on Rails";
+export const DESCRIPTION = "Notes as I'm learning Ruby on Rails";
+export const HOMEPAGE_URL = "https://joalloce.github.io/learning-ruby-on-rails";
+export const REPO_URL = "https://github.com/joalloce/learning-ruby-on-rails";
