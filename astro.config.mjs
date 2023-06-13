7c0e5d5ac1582bd0afe5099e3674d31e44acd9d5
Jose Lo <joalloce@gmail.com>
2023-06-13 08:02:05 -0600

    feat(sitemap): add sitemap feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,29 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import addClasses from "rehype-add-classes";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://joalloce.github.io",
   base: "/learning-ruby-on-rails",
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sitemap()],
   markdown: {
-    rehypePlugins: [
-      [
-        addClasses,
-        {
-          h1: "md-h1",
-          h2: "md-h2",
-          h3: "md-h3",
-          h4: "md-h3",
-          h5: "md-h5",
-          h6: "md-h6",
-          img: "md-img",
-          p: "md-p",
-          a: "md-a",
-          ul: "md-ul",
-          ol: "md-ol",
-          hr: "md-hr",
-        },
-      ],
-    ],
-  },
+    rehypePlugins: [[addClasses, {
+      h1: "md-h1",
+      h2: "md-h2",
+      h3: "md-h3",
+      h4: "md-h3",
+      h5: "md-h5",
+      h6: "md-h6",
+      img: "md-img",
+      p: "md-p",
+      a: "md-a",
+      ul: "md-ul",
+      ol: "md-ol",
+      hr: "md-hr"
+    }]]
+  }
 });
